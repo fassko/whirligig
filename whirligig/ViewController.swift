afd79270d16ca599b9045f0e434b5873cf3c5e1a
Kristaps Grinbergs <fassko@gmail.com>
2020-04-10 19:33:50 +0300

    Initial Commit

---
+++ whirligig/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whirligig
+//
+//  Created by Kristaps Grinbergs on 10/04/2020.
+//  Copyright © 2020 fassko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
